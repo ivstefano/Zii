d7f9b941cf3858b2669a1c251d3e0ebeb0ca54c5
istefanov <harbachek@gmail.com>
2014-12-01 22:21:05 +0100

    Error reporting added

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,4 +5,27 @@
  * Time: 9:16 PM
  */
 
-echo 'Hello World!';
+namespace Zii;
+
+use Whoops\Handler\PrettyPageHandler;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+    error_reporting(E_ALL);
+
+    $environment = 'development';
+
+    /**
+     * Register error handler
+     */
+
+    $woops = new \Whoops\Run;
+    if($environment !== 'production') {
+        $woops->pushHandler(new PrettyPageHandler());
+    } else {
+        $woops->pushHandler(function($e) {
+            echo 'Friendly error page and send an email to the developer';
+        });
+    }
+
+    $woops->register();
